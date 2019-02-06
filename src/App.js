8930441808b6bb5fc044c8df6d83e162165bd44e
project2401 <igor.it.step@gmail.com>
2019-02-06 16:08:36 +0200

    Give product at server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    prod:[]
   componentDidMount(){
+    fetch('http://smktesting.herokuapp.com/api/products/',{
+      method:'get'
+    })
+    .then(response => response.json()
+      .then(prod => this.setState({prod}))
+    )
+  }
+  
+  
   render() {
+    
+    
+    
     return (
         <div>
-		Hi
+            <div className='row'>
+             
+            </div>
         </div>
     );
   }
