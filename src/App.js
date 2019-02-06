ea04d66f72a8e599fee02e28c402eb7a43224763
project2401 <igor.it.step@gmail.com>
2019-02-06 15:53:53 +0200

    Create add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  componentDidMount(){
+  render() {
+    return (
+        <div>
+		Hi
+        </div>
+    );
+  }
+}
+
+export default App;
